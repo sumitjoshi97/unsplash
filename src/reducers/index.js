844e88b403e12109fd2d607b1bab4273b2b88a33
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-16 23:27:04 +0530

    Add pageReducer  to root Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import loadingReducer from './loadingReducer'
 import imagesReducer from './imagesReducer'
 import errorsReducer from './errorsReducer'
+import pageReducer from './pageReducer'
 
 const rootReducer = combineReducers({
   isLoading: loadingReducer,
   images: imagesReducer,
-  error: errorsReducer
+  error: errorsReducer,
+  nextPage: pageReducer
 })
 
 export default rootReducer
