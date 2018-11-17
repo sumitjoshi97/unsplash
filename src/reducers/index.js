086dd2e20a1c07c16a0bd0a99f5b271fb70f276b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-17 15:56:22 +0530

    Add stats reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import loadingReducer from './loadingReducer'
 import imagesReducer from './imagesReducer'
 import errorsReducer from './errorsReducer'
 import pageReducer from './pageReducer'
+import statsReducer from './statsReducer'
 
 const rootReducer = combineReducers({
   isLoading: loadingReducer,
   images: imagesReducer,
   error: errorsReducer,
-  nextPage: pageReducer
+  nextPage: pageReducer,
+  stats: statsReducer
 })
 
 export default rootReducer
