4772cb5cc7aa0a0bae00b063830cf37f02c6c657
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-17 15:55:41 +0530

    Add api fetch method to fetch image stats

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
-const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02'
+const KEY = `${process.env.REACT_APP_UNSPlASH_API}`
+const URL = 'https://api.unsplash.com/photos/'
 
 const fetchImages = async page => {
   const response = await fetch(
-    `https://api.unsplash.com/photos/?client_id=${key}&per_page=28&page=${page}`
+    `${URL}?client_id=${KEY}&per_page=28&page=${page}`
   )
   const data = await response.json()
   if (response.status > 400) {
@@ -11,4 +12,14 @@ const fetchImages = async page => {
   return data
 }
 
-export { fetchImages }
+const fetchImageStats = async id => {
+  const response = await fetch(`${URL}/?client_id=${KEY}/:${id}`)
+  const data = await response.json()
+  if (response.status > 400) {
+    throw new Error(data.errors)
+  }
+
+  return data
+}
+
+export { fetchImages, fetchImageStats }
